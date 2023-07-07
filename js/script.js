1ca94c3dc15eecea25264156bd8358e3280f863b
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-07-07 22:28:16 +0200

    pokemonList array created

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-
-alert('hello world');
-let favoriteFood= 'rice';
-document.write(favoriteFood);
+// array of pokemons
+let pokemonList = [
+    { name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+    { name: 'Butterfree', height: 1.1, type: ['bug', 'flying']},
+    { name: 'Raticate', height: 0.7, type: ['normal']}
+  ];
 
 
 
