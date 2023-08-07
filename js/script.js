455eb9188117ccfa65599feb7fcca6cbe5171025
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-08-07 23:43:32 +0200

    pokemonList array wrapped in an IIFE

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 // array of pokemons
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     { name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
     { name: 'Butterfree', height: 1.1, type: ['bug', 'flying']},
     { name: 'Raticate', height: 0.7, type: ['normal']}
   ];
 
-  // forEach loop 
-  pokemonList.forEach((pokemon) => {
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+function getAll() {
+    return pokemonList;
+}
+
+return {
+    add: add, 
+    getAll: getAll
+};
+})();
+
+//print to console
+console.log(pokemonRepository.getAll());
+
+  // forEach  loop 
+  pokemonRepository.getAll().forEach((pokemon) => {
     document.write(pokemon.name + " (height: " + pokemon.height + ")")
     // conditional if height is greater than 1.
     if (pokemon.height > 1.5) {
